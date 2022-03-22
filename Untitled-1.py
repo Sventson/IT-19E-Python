c3743b7e35f2367f868b44df067cabf8105a51c2
Sventson <101623007+Sventson@users.noreply.github.com>
2022-03-22 09:52:31 +0200

    abc

---
+++ Untitled-1.py
@@ -0,0 +1,42 @@
+ruut = input("P1 :")
+print (ruut)
+
+if ruut == "1":
+    print("X", "2", "3")
+    print("4", "5", "6")
+    print("7", "8", "9")
+if ruut == "2":
+    print("1", "X", "3")
+    print("4", "5", "6")
+    print("7", "8", "9")
+if ruut == "3":
+    print("1", "2", "X")
+    print("4", "5", "6")
+    print("7", "8", "9")
+if ruut == "4":
+    print("1", "2", "3")
+    print("X", "5", "6")
+    print("7", "8", "9")
+if ruut == "5":
+    print("1", "2", "3")
+    print("4", "X", "6")
+    print("7", "8", "9")
+if ruut == "6":
+    print("1", "2", "3")
+    print("4", "5", "X")
+    print("7", "8", "9")
+if ruut == "7":
+    print("1", "2", "3")
+    print("4", "5", "6")
+    print("X", "8", "9")
+if ruut == "8":
+    print("1", "2", "3")
+    print("4", "5", "6")
+    print("7", "X", "9")
+if ruut == "9":
+    print("1", "2", "3")
+    print("4", "5", "6")
+    print("7", "8", "X")
+
+ruut = input("P2 :")
+print (ruut)
